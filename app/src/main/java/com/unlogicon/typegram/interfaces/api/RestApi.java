c90c2409b4210da06064dded73dbb1750ba3ad55
Nikita Korovkin <vogster100@mail.ru>
2018-10-22 17:49:15 +0300

    Add cool toolbar and upload image

--- app/src/main/java/com/unlogicon/typegram/interfaces/api/RestApi.java
+++ app/src/main/java/com/unlogicon/typegram/interfaces/api/RestApi.java
@@ -9,12 +9,16 @@ import com.unlogicon.typegram.models.posts.PostRegister;
 import java.util.List;
 
 import io.reactivex.Observable;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
 import okhttp3.ResponseBody;
 import retrofit2.Response;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.Headers;
+import retrofit2.http.Multipart;
 import retrofit2.http.POST;
+import retrofit2.http.Part;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
 
@@ -84,4 +88,13 @@ public interface RestApi {
     @POST("editor/0")
     @Headers("Content-Type: application/json")
     Observable<Response<ResponseBody>> postArticle(@Body PostArticle article);
+
+    /**
+     * Загрузка карьинок
+     * @param image
+     * @return
+     */
+    @Multipart
+    @POST("upload")
+    Observable<Response<ResponseBody>> uploadImage(@Part MultipartBody.Part image);
 }
--- app/src/main/java/com/unlogicon/typegram/ui/activities/ArticleEditorActivity.java
+++ app/src/main/java/com/unlogicon/typegram/ui/activities/ArticleEditorActivity.java
@@ -1,18 +1,38 @@
 package com.unlogicon.typegram.ui.activities;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.Snackbar;
 import android.support.v7.widget.AppCompatEditText;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+import android.widget.ProgressBar;
 
 import com.arellomobile.mvp.MvpActivity;
 import com.arellomobile.mvp.MvpAppCompatActivity;
 import com.arellomobile.mvp.presenter.InjectPresenter;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 import com.unlogicon.typegram.R;
 import com.unlogicon.typegram.TgramApplication;
+import com.unlogicon.typegram.adapters.MainRecyclerViewAdapter;
+import com.unlogicon.typegram.adapters.MarkdownToolbarAdapter;
 import com.unlogicon.typegram.interfaces.activities.ArticleEditorView;
 import com.unlogicon.typegram.presenters.activities.ArticleEditorPresenter;
+
+import java.io.File;
 
 /**
  * Nikita Korovkin 18.10.2018.
@@ -23,14 +43,27 @@ public class ArticleEditorActivity extends MvpAppCompatActivity implements Artic
 
     private AppCompatEditText title, ogimage, body, tag;
 
+    public static final int PICK_IMAGE = 100;
+
+    private Snackbar preogressSnackBar;
+
+    private RecyclerView markdownToolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
         setContentView(R.layout.activity_article_editor);
+
+
+        Toolbar myToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
 
+        setProgressBarIndeterminateVisibility(true);
 
         title = findViewById(R.id.title);
         ogimage = findViewById(R.id.ogimage);
@@ -41,6 +74,15 @@ public class ArticleEditorActivity extends MvpAppCompatActivity implements Artic
         presenter.setOgimageTextWatcher(ogimage);
         presenter.setBodyTextWatcher(body);
         presenter.setTagTextWatcher(tag);
+
+        markdownToolbar = findViewById(R.id.markdown_toolbar);
+        markdownToolbar.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+
+
+
+        preogressSnackBar = Snackbar.make(body, "Uploading...", Snackbar.LENGTH_INDEFINITE);
+        Snackbar.SnackbarLayout snack_view = (Snackbar.SnackbarLayout) preogressSnackBar.getView();
+        snack_view.addView(new ProgressBar(this));
     }
 
     @Override
@@ -75,6 +117,47 @@ public class ArticleEditorActivity extends MvpAppCompatActivity implements Artic
     }
 
     @Override
+    public void openImage() {
+
+        Dexter.withActivity(this)
+                .withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                .withListener(new PermissionListener() {
+                    @Override public void onPermissionGranted(PermissionGrantedResponse response) {
+                        Intent i = new Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                        startActivityForResult(Intent.createChooser(i, "Select Image"), PICK_IMAGE);
+                    }
+                    @Override public void onPermissionDenied(PermissionDeniedResponse response) {showSnackbar(getString(R.string.need_permissions));}
+                    @Override public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+                        token.continuePermissionRequest();
+                    }
+                }).check();
+
+    }
+
+    @Override
+    public void showProgressSnackBar() {
+        preogressSnackBar.show();
+    }
+
+    @Override
+    public void hideProgressSnackBar() {
+        preogressSnackBar.dismiss();
+    }
+
+    @Override
+    public void setMarkdownToolbarAdapter(MarkdownToolbarAdapter markdownToolbarAdapter) {
+        markdownToolbar.setAdapter(markdownToolbarAdapter);
+    }
+
+    @Override
+    public void insertTextTotBody(String text) {
+        int start = Math.max(body.getSelectionStart(), 0);
+        int end = Math.max(body.getSelectionEnd(), 0);
+        body.getText().replace(Math.min(start, end), Math.max(start, end),
+                text, 0, text.length());
+    }
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_editor, menu);
         return true;
@@ -97,5 +180,11 @@ public class ArticleEditorActivity extends MvpAppCompatActivity implements Artic
     public boolean onSupportNavigateUp() {
         onBackPressed();
         return true;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        presenter.onActivityResult(requestCode, resultCode, data);
     }
 }
